33ad1ae8c23b6111df96c05d297d925002066be1
faisal004 <faisalhusain1320@gmail.com>
2024-07-07 19:45:30 +0530

    added : new components added

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -1,15 +1,131 @@
-import { Plus } from "lucide-react";
+'use client'
+import { Plus } from 'lucide-react'
+import * as React from 'react'
 
-const Footer = () => {
-    return ( <div className="bg-[#EEE9FF] fixed bottom-0 p-5 w-full flex items-center justify-center h-12 ">
-        <div>
-        <div className="bg-red-200 p-5  -translate-y-6">
-<Plus className="h-7 w-7"/>
-        </div>
+import { cn } from '@/lib/utils'
+import { Button } from '@/components/ui/button'
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog'
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from '@/components/ui/drawer'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { useMediaQuery } from 'react-responsive'
+export function DrawerDialogDemo() {
+  const [open, setOpen] = React.useState(false)
+  const isDesktop = useMediaQuery({
+    query: '(min-width: 768px)',
+  })
 
-        </div>
- 
-    </div> );
+  if (isDesktop) {
+    return (
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogTrigger asChild>
+          <Button variant="outline">Edit Profile</Button>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle>Edit profile</DialogTitle>
+            <DialogDescription>
+              Make changes to your profile here. Click save when youre done.
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+    )
+  }
+
+  return (
+    <Drawer open={open} onOpenChange={setOpen}>
+      <DrawerTrigger asChild>
+        <Button variant="outline">Edit Profile</Button>
+      </DrawerTrigger>
+      <DrawerContent>
+        <DrawerHeader className="text-left">
+          <DrawerTitle>Edit profile</DrawerTitle>
+          <DrawerDescription>
+            Make changes to your profile here. Click save when youre done.
+          </DrawerDescription>
+        </DrawerHeader>
+
+        <DrawerFooter className="pt-2">
+          <DrawerClose asChild>
+            <Button variant="outline">Cancel</Button>
+          </DrawerClose>
+        </DrawerFooter>
+      </DrawerContent>
+    </Drawer>
+  )
 }
- 
-export default Footer;
+const Footer = () => {
+  const [open, setOpen] = React.useState(false)
+  const isDesktop = useMediaQuery({
+    query: '(min-width: 768px)',
+  })
+  if (isDesktop) {
+    return (
+      <div className="bg-[#EEE9FF] fixed bottom-0 p-5 w-full flex items-center justify-center h-16 rounded-t-2xl ">
+        <Dialog open={open} onOpenChange={setOpen}>
+          <DialogTrigger asChild>
+            <div>
+              <div className="bg-purpleapp p-1  -translate-y-8 text-white rounded-full cursor-pointer hover:bg-purpleapp/90">
+                <Plus className="md:h-16 md:w-16 h-14 w-14" />
+              </div>
+            </div>
+          </DialogTrigger>
+          <DialogContent className="sm:max-w-[425px]">
+            <DialogHeader>
+              <DialogTitle>Edit profile</DialogTitle>
+              <DialogDescription>
+                Make changes to your profile here. Click save when youre done.
+              </DialogDescription>
+            </DialogHeader>
+          </DialogContent>
+        </Dialog>
+      </div>
+    )
+  }
+  return (
+    <div className="bg-[#EEE9FF] fixed bottom-0 p-5 w-full flex items-center justify-center h-16 rounded-t-2xl ">
+      <Drawer open={open} onOpenChange={setOpen}>
+        <DrawerTrigger asChild>
+          <div>
+            <div className="bg-purpleapp p-1  -translate-y-6 text-white rounded-full cursor-pointer hover:bg-purpleapp/90">
+              <Plus className="md:h-16 md:w-16 h-14 w-14" />
+            </div>
+          </div>
+        </DrawerTrigger>
+        <DrawerContent className='h-[800px]'>
+          <DrawerHeader className="text-left">
+            <DrawerTitle>Edit profile</DrawerTitle>
+            <DrawerDescription>
+              Make changes to your profile here. Click save when youre done.
+            </DrawerDescription>
+          </DrawerHeader>
+
+          <DrawerFooter className="pt-2">
+            <DrawerClose asChild>
+              <Button variant="outline">Cancel</Button>
+            </DrawerClose>
+          </DrawerFooter>
+        </DrawerContent>
+      </Drawer>
+    </div>
+  )
+}
+
+export default Footer
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,6 +19,7 @@ const config = {
     },
     extend: {
       colors: {
+        purpleapp:"#5F33E1",
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
